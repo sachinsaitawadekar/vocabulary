3991774191d7a65f5b0effe0b8d4a068a8b25cc9
sachinsaitawadekar <sachinsaitawadekar@gmail.com>
2025-10-01 16:22:03 +0530

    adding one more filter to discount

--- registrations.php
+++ registrations.php
@@ -45,7 +45,7 @@ function e($value) {
 }
 
 $stageOptions = ['NIL', 'Prospect', 'Enrolled', 'Lost'];
-$discountOptions = ['NIL', '10%', '20%', '30%', '40%'];
+$discountOptions = ['NIL', 'NA', '10%', '20%', '30%', '40%'];
 $referenceOptions = ['NIL', 'Pamphlete', 'Direct', 'Social Media', 'Word of Mouth'];
 
 $allowedSort = [
